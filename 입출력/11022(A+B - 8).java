be3e80f8210943c3ec7dd373b336bd5a8f763936
IslandofDream <rhffldkt0517@naver.com>
2021-01-06 16:41:29 +0900

    Create 11022(A+B - 8).java

---
+++ 입출력/11022(A+B - 8).java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n ;
+        n = sc.nextInt();
+
+        for(int i=0; i<n; i++) {
+
+
+                int a ,b;
+
+                a = sc.nextInt();
+                if(a <0 || a >9){
+                    break;
+                }
+                b = sc.nextInt();
+                if(b <0 || b> 9){
+                    break;
+                }
+
+                if (a == 0 && b == 0) {
+                    break;
+                }
+                System.out.println("Case #"+(i+1) +": " + a + " + " + b + " = "+ (a + b));
+
+        }
+    }
+}
