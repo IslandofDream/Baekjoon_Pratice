2516dacadf44baab605415692fa411e79d98ba0b
IslandofDream <rhffldkt0517@naver.com>
2021-01-06 16:47:31 +0900

    Create 2439(별 찍기 - 2).java

---
+++ 입출력/2439(별 찍기 - 2).java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n;
+        n = sc.nextInt();
+
+        for(int i = 0; i<n; i++){
+            for(int j = n-1; j>-1; j--) {
+                if(j > i){
+                    System.out.print(" ");
+                }
+                else{System.out.print("*");}
+
+            }
+            System.out.println("");
+        }
+
+    }
+}
+
