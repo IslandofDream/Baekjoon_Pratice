818095d693e98043ad49b5f6ee4f528dc6054ed6
IslandofDream <rhffldkt0517@naver.com>
2021-01-06 21:23:21 +0900

    Create 11727_2*n타일링2.java

---
+++ DP/11727_2*n타일링2.java
@@ -0,0 +1,32 @@
+
+import java.util.Scanner;
+
+public class Main {
+
+    static int result[] = new int[1001];
+
+    public static int domino(int n){
+
+        result[0]= 1;
+        result[1]= 1;
+        result[2] = 3;
+        
+        if(result[n]!=0) {
+            return result[n];
+        }
+        else{
+            result[n] = (domino(n - 1) + domino(n - 2)+ domino(n - 2)) % 10007;
+        }
+
+        return  result[n];
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.print(domino(n));
+
+    }
+}
+
