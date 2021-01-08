f0e96656a6a5efdb41ad777f56fd1283fdb7fe10
IslandofDream <rhffldkt0517@naver.com>
2021-01-09 00:07:37 +0900

    Create 11053_가장긴증가하는부분수열.java

---
+++ DP/11053_가장긴증가하는부분수열.java
@@ -0,0 +1,42 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+        int t = Integer.parseInt(br.readLine());
+
+        int number [] = new int[t];
+        int dp[] = new int[t];
+
+        int max = 0;
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i<t; i++){
+            number[i] = Integer.parseInt(st.nextToken());
+        }
+
+
+        for(int i = 0; i<t; i++){
+            dp[i] = 1;
+            for(int j = 0; j<i; j++){
+                if(number[i] > number[j] && dp[i] < dp[j]+1){
+                    dp[i] = dp[j] + 1 ;
+                }
+
+            }
+            
+            if(max < dp[i]){
+                max = dp[i];
+            }
+        }
+
+        System.out.println(max);
+
+    }
+}
+
+
